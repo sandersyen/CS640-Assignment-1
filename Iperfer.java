f98bcdb25d516745831e4020d24e1b29b50ed3f3
Frank Hong <frankied4c@gmail.com>
2018-09-21 17:30:41 -0500

    main(part of)

--- Iperfer.java
+++ Iperfer.java
@@ -2,21 +2,36 @@ public class Iperfer {
 
 	public static void main(String[] args) {
 		if (args.length == 0) {
-			System.out.println("Error:");
+			System.out.println("Error: missing or additional arguments");
 			System.exit(-1);
 		}
 	    
 		String mode = args[0];
 		
-		if (mode == "-c") {
+		if (mode.equals("-c")) {
 			
 		}
-		else if (mode == "-s") {
+		else if (mode.equals("-c")) {
+			if (args.length == 3) & (args[1].equals("-p")) {
+				int port = Integer.parseInt(arg[2]);
+				if (port >= 1024) & (port <= 65535) {
+					Server Iperf_Server= new Server(port);
+					Iperf_Server.execute();
+				}
+				else {
+					System.out.println("Error: port number must be in the range 1024 to 65535");
+					System.exit(-1);
+				}
+			}
+			else {
+				System.out.println("Error: missing or additional arguments");
+				System.exit(-1);
+			}
 
 		}
 		
 		else {
-			System.out.println("Error:");
+			System.out.println("Error: missing or additional arguments");
 			System.exit(-1);
 		}
 	}
