afbff2e1c2c2b00a9f2b56d34515656dab7963e2
Frank Hong <frankied4c@gmail.com>
2018-09-22 19:39:35 -0500

    some little fixes

--- Iperfer.java
+++ Iperfer.java
@@ -5,15 +5,15 @@ public class Iperfer {
 			System.out.println("Error: missing or additional arguments");
 			System.exit(-1);
 		}
-	    
+
 		String mode = args[0];
-		
+
 		if (mode.equals("-c")) {
 			if ((args.length == 7) && args[1].equals("-h") && args[3].equals("-p") && args[5].equals("-t")) {
 				int port = Integer.parseInt(args[4]);
 				double time = Double.parseDouble(args[6]);
 				if ((port >= 1024) && (port <= 65535)) {
-					IperfClient Iperf_Server= new IperfClient(args[2], port, time);
+					IperfClient Iperf_Server = new IperfClient(args[2], port, time);
 				}
 				else {
 					System.out.println("Error: port number must be in the range 1024 to 65535");
@@ -25,12 +25,12 @@ public class Iperfer {
 				System.exit(-1);
 			}
 		}
+
 		else if (mode.equals("-s")) {
 			if ((args.length == 3) && args[1].equals("-p")) {
 				int port = Integer.parseInt(args[2]);
 				if ((port >= 1024) && (port <= 65535)) {
-					Server Iperf_Server= new Server(port);
-					Iperf_Server.execute();
+					IperfServer Iperf_Server = new IperfServer(port);
 				}
 				else {
 					System.out.println("Error: port number must be in the range 1024 to 65535");
@@ -41,7 +41,7 @@ public class Iperfer {
 				System.out.println("Error: missing or additional arguments");
 				System.exit(-1);
 			}
-		}		
+		}
 		else {
 			System.out.println("Error: missing or additional arguments");
 			System.exit(-1);
