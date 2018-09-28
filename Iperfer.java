e0cee87fd9fbb98bffa54d314498c41d893c576e
sandersyen <sandersyen37@gmail.com>
2018-09-27 20:16:06 -0500

    handle port exception

--- Iperfer.java
+++ Iperfer.java
@@ -10,8 +10,16 @@ public class Iperfer {
 
 		if (mode.equals("-c")) {
 			if ((args.length == 7) && args[1].equals("-h") && args[3].equals("-p") && args[5].equals("-t")) {
-				int port = Integer.parseInt(args[4]);
-				double time = Double.parseDouble(args[6]);
+				int port = 10000;
+				double time = 30;
+				try {
+					port = Integer.parseInt(args[4]);
+					time = Double.parseDouble(args[6]);	
+				} catch (Exception e) {
+					System.err.println("Parsing argument failed");
+					System.exit(1);
+				}
+
 				if ((port >= 1024) && (port <= 65535)) {
 					IperfClient Iperf_Server = new IperfClient(args[2], port, time);
 				}
@@ -28,7 +36,14 @@ public class Iperfer {
 
 		else if (mode.equals("-s")) {
 			if ((args.length == 3) && args[1].equals("-p")) {
-				int port = Integer.parseInt(args[2]);
+				int port = 10000;
+				try {
+					port = Integer.parseInt(args[2]);	
+				} catch (Exception e) {
+					System.err.println("Parsing argument failed");
+					System.exit(1);
+				}
+				
 				if ((port >= 1024) && (port <= 65535)) {
 					IperfServer Iperf_Server = new IperfServer(port);
 				}
